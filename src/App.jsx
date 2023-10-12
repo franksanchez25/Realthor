6b5fc3737a876a5b4b435788bf5db09c56297c48
Frank Sanchez <fesd2501@gmail.com>
2023-10-11 21:00:55 -0400

    Header component added

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ import { SignIn } from './pages/SignIn'
 import { SignOut } from './pages/SignOut'
 import { Profile } from './pages/Profile'
 import { About } from './pages/about'
+import { Header } from './components/Header'
 export const App = () => {
   return (
      <BrowserRouter>
+
+      <Header/>
+
         <Routes>
           <Route path='/' element={<Home/>} />     
           <Route path='/sign-in' element={<SignIn/>} />     
